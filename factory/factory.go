8553b997fbd352faababafcc92877771a95ee64b
HeTaixiang <nhhtx@163.com>
2017-04-06 18:13:33 +0800

    format import

--- factory/factory.go
+++ factory/factory.go
@@ -1,35 +1,38 @@
 package factory
 
 import (
-	"github.com/emicklei/go-restful"
 	"sync"
+
+	"github.com/emicklei/go-restful"
 	"github.com/golang/glog"
 )
 
+// Factory is type of the webservice register factory function
 type Factory func(container *restful.Container)
 
 var (
 	serviceMutex sync.Mutex
-	services = make(map[string]Factory)
+	services     = make(map[string]Factory)
 )
 
-
-func RegisterService(path string, service Factory)  {
+// RegisterService is for register webservice
+func RegisterService(path string, service Factory) {
 	serviceMutex.Lock()
 	defer serviceMutex.Unlock()
 	_, found := services[path]
 	if found {
-		glog.Fatal("service factory %q has been registered", path)
+		glog.Errorf("service factory %q has been registered", path)
 	}
 	glog.V(1).Infof("registered service %q", path)
 	services[path] = service
 }
 
-func GetServices()[]Factory  {
+// GetServices is get all register webservice
+func GetServices() []Factory {
 	serviceMutex.Lock()
 	defer serviceMutex.Unlock()
 	wsServices := []Factory{}
-	for _, s := range services{
+	for _, s := range services {
 		wsServices = append(wsServices, s)
 	}
 	return wsServices
