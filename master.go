df0e1f6bca493bf4aed9e2fe26506476606042e6
HeTaixiang <nhhtx@163.com>
2017-04-06 18:04:12 +0800

    add log

--- master.go
+++ master.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-	"github.com/emicklei/go-restful"
-	factory "github.com/HeTaixiang/master/factory"
-	_ "github.com/HeTaixiang/master/controller"
+	"flag"
 	"net/http"
+
+	_ "github.com/HeTaixiang/master/controller"
+	factory "github.com/HeTaixiang/master/factory"
+	"github.com/emicklei/go-restful"
 	"github.com/golang/glog"
 )
 
 func main() {
+	flag.Parse()
+	defer glog.Flush()
+
 	glog.V(1).Infof("create restful container...")
 	container := restful.NewContainer()
 	for _, service := range factory.GetServices() {
 		service(container)
 	}
-	server := &http.Server{Addr:":8080", Handler:container}
+	server := &http.Server{Addr: ":8080", Handler: container}
 	glog.Fatal(server.ListenAndServe())
 
 }
