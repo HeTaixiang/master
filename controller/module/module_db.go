79f5270648a8c94f7d271cbbaec67f7bd0a8c497
HeTaixiang <nhhtx@163.com>
2017-04-13 23:58:25 +0800

    update db

--- controller/module/module_db.go
+++ controller/module/module_db.go
@@ -6,9 +6,19 @@ import (
 
 // Manager used to manage Module with database
 type Manager interface {
-	Get() []*controller.Module
+	Get() ([]*controller.Module, error)
 
-	GetSubModulesByID(ID int64) []*controller.SubModule
+	GetByID(ID int64) (*controller.Module, error)
+
+	GetSubModulesByModuleID(ID int64) ([]*controller.SubModule, error)
+
+	GetSubModuleByID(ID int64) (*controller.SubModule, error)
+
+	Add(module *controller.Module) error
+
+	Update(module *controller.Module) error
+
+	DeleteByID(ID int16) error
 }
 
 type moduleManager struct {
